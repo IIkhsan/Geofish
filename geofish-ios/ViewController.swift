cb7fabe155a6ec63504043b77efd0c5f3e88af67
Ильяс <ilias.ihsanov@gmail.com>
2017-10-16 09:36:14 +0300

    Initial Commit

---
+++ geofish-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  geofish-ios
+//
+//  Created by Ilyas on 16.10.17.
+//  Copyright © 2017 Ilyas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
