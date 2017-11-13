6c579728172b141b6e40124e45e145102ddd7762
Ильяс <ilias.ihsanov@gmail.com>
2017-11-13 20:00:18 +0300

    Add email check in registration and add VK user information download but don't register

--- geofish-ios/Login page/SignUpVC.swift
+++ geofish-ios/Login page/SignUpVC.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
+import SVProgressHUD
 import SkyFloatingLabelTextField
+import Alamofire
 
 class SignUpVC: UIViewController {
     @IBOutlet weak var emailTextField: SkyFloatingLabelTextField!
@@ -18,7 +20,6 @@ class SignUpVC: UIViewController {
         super.viewDidLoad()
         self.navigationController?.clearColor()
         self.setBackButton()
-        
         emailTextField.addTarget(self, action: #selector(textFieldDidChange(textField:)), for: .editingChanged)
         // Do any additional setup after loading the view.
     }
@@ -29,8 +30,16 @@ class SignUpVC: UIViewController {
         
         if  isValidEmail(testStr: emailTextField.text!) {
             print("valid")
-            user.email = self.emailTextField.text
-            performSegue(withIdentifier: "showNamePassword", sender: nil)
+            SVProgressHUD.setDefaultMaskType(.clear)
+            checkDuplicateEmail(email: self.emailTextField.text!, completion: { (result) in
+                if result{
+                    SVProgressHUD.showError(withStatus: "Такой email уже существует")
+                }else{
+                    SVProgressHUD.showSuccess(withStatus: nil)
+                    self.performSegue(withIdentifier: "showNamePassword", sender: nil)
+                }
+                SVProgressHUD.dismiss(withDelay: 0.3)
+            })
         }else{
             emailTextField.errorMessage = "Неверный формат email"
             emailTextField.updateColors()
@@ -49,6 +58,31 @@ class SignUpVC: UIViewController {
         
         let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
         return emailTest.evaluate(with:testStr)
+    }
+    
+    func checkDuplicateEmail(email: String, completion: @escaping (Bool) -> Void){
+        let values = ["email" : "\(email)"]
+        let header = ["content-type" : "application/json"]
+        SVProgressHUD.show(withStatus: "Загрузка...")
+        Alamofire.request("https://geofish.herokuapp.com/api/v1/users/already_exist", method: .get, parameters: values, headers: header).responseJSON { (response) in
+            print(response)
+            if let status = response.response?.statusCode {
+                switch(status){
+                case 200:
+                    print("Запрос успешный")
+                default:
+                    print("Ошибка запроса: \(status)")
+                }
+            }
+            //to get JSON return value
+            if let data = response.result.value {
+                let JSON = data as! NSDictionary
+                let exist = JSON.object(forKey: "exist") as! Bool
+                self.user.email = self.emailTextField.text
+                print("Exist: \(exist)")
+                completion(exist)
+            }
+        }
     }
     
     
--- geofish-ios/Login page/WelcomeVC.swift
+++ geofish-ios/Login page/WelcomeVC.swift
@@ -8,57 +8,80 @@
 
 import UIKit
 import VK_ios_sdk
+import SVProgressHUD
 
-class WelcomeVC: UIViewController, VKSdkDelegate, VKSdkUIDelegate {
+class WelcomeVC: UIViewController, VKSdkDelegate, VKSdkUIDelegate, UIAlertViewDelegate {
+    var SCOPE: Array<Any>!
+    let VK_APP_ID = "6251653"
+    let SELECT_VIEWCONTROLLER = "LoginSeque"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        SCOPE = [VK_PER_FRIENDS, VK_PER_WALL, VK_PER_PHOTOS, VK_PER_EMAIL]
+        self.navigationController?.clearColor()
+        self.navigationController?.setBackButton()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        vkInit()
+    }
+    
+    func vkInit(){
+        SVProgressHUD.setDefaultMaskType(.clear)
+        SVProgressHUD.show(withStatus: "Загрузка...")
+        VKSdk.initialize(withAppId: VK_APP_ID).register(self)
+        VKSdk.instance().uiDelegate = self
+        VKSdk.wakeUpSession(SCOPE) { (state, error) in
+            if state == VKAuthorizationState.authorized{ //если пользователь авторизован
+                self.selectToRegisterOrAuthorizaton()
+                //сессия с пользователем сохранена, нет необходимости заправшивать доступ
+            }else if (error != nil) {
+                self.alertMessage()
+            }
+        }
+        SVProgressHUD.dismiss(withDelay: 0.5)
+    }
+    
+    //Если пользователь уже авторизован в сессии то переход к странице
+    func selectToRegisterOrAuthorizaton(){
+        self.performSegue(withIdentifier: SELECT_VIEWCONTROLLER, sender: self)
+    }
+    
     func vkSdkShouldPresent(_ controller: UIViewController!) {
         self.navigationController?.topViewController?.present(controller, animated: true, completion: nil)
     }
     
     func vkSdkNeedCaptchaEnter(_ captchaError: VKError!) {
-        
+        let vc = VKCaptchaViewController.captchaControllerWithError(captchaError)
+        vc?.present(in: self.navigationController?.topViewController)
     }
     
     func vkSdkAccessAuthorizationFinished(with result: VKAuthorizationResult!) {
-        if ((result) != nil){
-            print(result.user.id)
-        }else{
-            print(result.error)
+        if (result.token != nil){
+            self.selectToRegisterOrAuthorizaton()
+        }else if (result.error != nil){
+            UIAlertController.init(title: "Внимание!", message: "Ошибка авторизации", preferredStyle: .alert).show(self, sender: self)
         }
     }
     
     func vkSdkUserAuthorizationFailed() {
-        
+        UIAlertController(title: nil, message: "Ошибка авторизации", preferredStyle: .alert).show(self, sender: self)
     }
     
-    let VK_APP_ID = "6251653"
-    let SCOPE = ["email", "photos"]
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.navigationController?.clearColor()
-        self.navigationController?.setBackButton()
-        let sdkInstance = VKSdk.initialize(withAppId: self.VK_APP_ID)
-        sdkInstance?.register(self);
-        sdkInstance?.uiDelegate = self;
-        VKSdk.wakeUpSession(SCOPE) { (state, error) in
-            if state == VKAuthorizationState.authorized{
-                
-            }else{
-                print(error)
-            }
-        }
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     @IBAction func vkAutorizateButtonAction(_ sender: Any) {
-        
         VKSdk.authorize(SCOPE)
     }
+    
     @IBAction func okAutorizeButtonAction(_ sender: Any) {
         
+    }
+    
+    func alertMessage(){
+        let alert = UIAlertController(title: "Внимание!", message: "Проверьте подключение к интернету и состояние сети", preferredStyle: .actionSheet)
+        let defaultAction = UIAlertAction(title: "Понятно", style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
